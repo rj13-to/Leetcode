78176576f6a60da960eb38a8edef0d62d021669b
Prabhdeep Singh <73354129+rj13-to@users.noreply.github.com>
2021-08-21 15:17:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.34%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<3) return n;
+        int a=1,b=2;
+        for(int i=3;i<=n;i++){
+            b=b+a;
+            a=b-a;
+        }
+        return b;
+    }
+};
